5a14d5d36d6a7bff8a85199780b3f523f12c002e
Axel Carapinha <axelcarapinha20@gmail.com>
2024-08-01 16:07:36 +0100

    Backend receiving the data from the form

---
+++ 3_Simple-interface/config.py
@@ -0,0 +1,6 @@
+MIN_LEN_PASSWORD = 15 
+# Even though NIST recommends >8 for this case (topic 5.1.1.1)
+# https://pages.nist.gov/800-63-3/sp800-63b.html
+
+MIN_LEN_EMAIL = 4
+MIN_LEN_USERNAME = 8
--- 3_Simple-interface/website/authn.py
+++ 3_Simple-interface/website/authn.py
@@ -1,16 +1,37 @@
-from flask import Blueprint
+import config  
+from flask import Blueprint, render_template, request, flash
 
 authn = Blueprint('authn', __name__)
 
-
-@authn.route('/login')
+@authn.route('/login', methods=['GET', 'POST'])
 def login(): # can have a name diff from the route one
-    return "<p>Login</p>"
+    data = request.form # ALL the info sent from the form
+    print(data) 
+    return render_template("login.html")
 
 @authn.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("home.html")
 
-@authn.route('/sign-up')
+@authn.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign_up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password_first = request.form.get('passwordFirst')
+        password_confirmation = request.form.get('passwordConfirmation')
+
+        if len(email) < config.MIN_LEN_EMAIL:
+            flash(f'Email must be greather than {config.MIN_LEN_EMAIL - 1} characters.', category='error')
+        elif len(username) < config.MIN_LEN_USERNAME:
+            flash(f'First name should be greather than {config.MIN_LEN_USERNAME - 1} characters.', category='error')
+        elif password_first != password_confirmation:
+            flash('Passwords do NOT match.', category='error')
+        elif len(password_first) < config.MIN_LEN_PASSWORD: #TODO add more rules and tips for the password field
+            flash(f'Password should have at least {config.MIN_LEN_PASSWORD - 1} characters.', category='error')
+        else:
+            flash("Account creation SUCCESSFUL", category='success')
+            # add user to database
+
+
+    return render_template("sign_up.html")
