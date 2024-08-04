4bc714253a12db396a68292026ccc78100b58b5b
Axel Carapinha <axelcarapinha20@gmail.com>
2024-08-04 22:56:32 +0100

    Logo created and added

--- 2_API-LLM-Word/llm_prompt.py
+++ 2_API-LLM-Word/llm_prompt.py
@@ -3,20 +3,21 @@ from langchain_core.prompts import ChatPromptTemplate
 
 def get_word_info(user_context, word):
     template = """
-    Follow the instrcutions below, considering you are
-    an expert on english vocabulary.
+    user_context: {user_context}
+    word (correct if misspelled): {word}
 
-    Consider this word: {word}
-    Consider this profile of the user: {user_context}
-
-    Answer me just with 5 lines:
-    the first for the meaning of the provided word,
-    the other 4 for a simple dialog using the word
-    considering the provided context of the user
-
-    If the word does NOT exist, send empty JSON.
-
-    Answer just with JSON format.
+    Answer only with the JSON in the following format:
+    
+        definition: definition of the word,
+        dialog (a JSON array, with 6 sentences): 
+            person1: SENTENCE 1,
+            person2: SENTENCE 2
+        
+            person1: SENTENCE 3,
+            person2: SENTENCE 4
+        
+            person1: SENTENCE 5,
+            person2: SENTENCE 6
     """
 
     model = OllamaLLM(model="llama3")
@@ -24,10 +25,9 @@ def get_word_info(user_context, word):
     chain = prompt | model # the model will be invoked automatically (the pipe is similar to Linux)
 
     result = chain.invoke({"user_context": {user_context}, "word": {word}})
+    print(result) #TODO remove
     return result
-
-
 
 # user_context = "Computer Networks and Cybersecurity"
 # word = "passion"
-# print(get_word_info_json(user_context, word))
+# print(get_word_info(user_context, word))
