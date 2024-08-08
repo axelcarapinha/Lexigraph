32dfd01ef50cc768b498b885278779ec99a80e14
Axel Carapinha <axelcarapinha20@gmail.com>
2024-08-08 15:54:14 +0100

    Testing if othther folders were the source of the errors during the Azure deployment

--- 5_Telegram-bot/config.py
+++
@@ -1,6 +0,0 @@
-POLL_INTERVAL = 3
-URL_LEXIGRAPH_WEBSITE = "http://127.0.0.1"
-URL_API_ANKI = "http://127.0.0.1:8765"
-
-BASE_API_WORD = "http://127.0.0.1:5001/" # base URL for the API responsible for word info
-DECK_NAME = 'Default'
